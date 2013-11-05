c07d465ac8fca2cc98cb029c34cd88c28a96f687
Markus Emrich <Markus.Emrich@gmail.com>
2013-11-05 11:09:09 +0100

    refactoring + tap gesture recognizer to open & close side menu

--- StatusBarTest/SHStatusBarViewController.m
+++ StatusBarTest/SHStatusBarViewController.m
@@ -9,71 +9,64 @@
 #import "SHStatusBarViewController.h"
 
 @interface SHStatusBarViewController ()
-
-@property (nonatomic, assign) BOOL hideStatusBar;
-@property (nonatomic, strong) UIView *container;
-
+@property (nonatomic, assign) BOOL statusBarHidden;
+@property (nonatomic, strong) UIView *containerView;
+@property (nonatomic, strong) UIView *lastSnapShotView;
 @end
 
 @implementation SHStatusBarViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-
-        _hideStatusBar = NO;
-
-        _container = [[UIView alloc] initWithFrame:self.view.bounds];
-        [self.view addSubview:_container];
-
-
-        self.view.backgroundColor = [UIColor redColor];
-
-        [self performSelector:@selector(doSlideOut) withObject:nil afterDelay:2.0];
-    }
-    return self;
-}
-
-- (void)viewDidLoad
+- (void)viewDidLoad;
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor cyanColor];
+    
+    _containerView = [[UIView alloc] initWithFrame:self.view.bounds];
+    _containerView.backgroundColor = [UIColor magentaColor];
+    [self.view addSubview:_containerView];
+    
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc]
+                                     initWithTarget:self
+                                     action:@selector(showHideView:)]];
 }
 
-- (void)didReceiveMemoryWarning
+- (UIView *)snapShotView
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.lastSnapShotView = [[UIScreen mainScreen] snapshotViewAfterScreenUpdates:NO];
+    return self.lastSnapShotView;
 }
 
-- (UIView *)getSnapShot
+- (void)showHideView:(UITapGestureRecognizer*)recognizer
 {
-    return [[UIScreen mainScreen] snapshotViewAfterScreenUpdates:NO];
-}
-
-- (void)doSlideOut
-{
-
-    [self.container addSubview:[self getSnapShot]];
-    self.hideStatusBar = YES;
-    [self setNeedsStatusBarAppearanceUpdate];
-    self.view.backgroundColor = [UIColor clearColor];
-    [UIView animateWithDuration:0.5 animations:^{
-        [self.container setFrame:(CGRect){
-                200,
-                0,
-                self.container.frame.size
+    if (self.containerView.frame.origin.x > 0) {
+        [UIView animateWithDuration:0.4 animations:^{
+            [self.containerView setFrame:self.containerView.bounds];
+        } completion:^(BOOL finished) {
+            self.statusBarHidden = NO;
+            [self.lastSnapShotView removeFromSuperview];
+            self.lastSnapShotView = nil;
         }];
-    }];
-
+    } else {
+        [self.containerView addSubview:[self snapShotView]];
+        self.statusBarHidden = YES;
+        [UIView animateWithDuration:1.2 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:1.0 options:0 animations:^{
+            [self.containerView setFrame:CGRectOffset(self.containerView.bounds, 240.0, 0.0)];
+        } completion:nil];
+    }
 }
 
+- (void)setStatusBarHidden:(BOOL)statusBarHidden;
+{
+    if (_statusBarHidden != statusBarHidden) {
+        _statusBarHidden = statusBarHidden;
+        [self setNeedsStatusBarAppearanceUpdate];
+    }
+}
 
 - (BOOL)prefersStatusBarHidden
 {
-    return self.hideStatusBar;
+    return self.statusBarHidden;
 }
 
 @end
